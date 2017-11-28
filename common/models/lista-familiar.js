9818f401edb5283eb53677907818a00939a2dffd
Jose Manuel Zapata Martínez <joseicew@gmail.com>
2017-11-28 10:26:13 +0100

    Propietario de una lista Terminado

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -3,17 +3,26 @@
 module.exports = function(Listafamiliar) {
     console.log();
     Listafamiliar.afterRemote('create', function(context, listaFamiliar, next) {
-        //preparo el modelo listaFamiliar
-        var ListaFamiliar = listaFamiliar.app;
+        //preparo el modelo usuarios
+        var Usuarios = Listafamiliar.app.models.Usuario;
+        var listaCreada = listaFamiliar.id;
         // Compruebo si está logueado
-        console.log(context.req.accessToken);
+        
         if (context.req.accessToken && context.req.accessToken.userId) {
             //Si está logueado le asocio a esa lista que acaba de crear su id como owner
             
-            console.log("El usuario existe");
             context.args.data.ownerId = context.req.accessToken.userId;
-            console.log(context.args.data.ownerId);
-            next();
+            console.log(listaFamiliar.ownerId);
+            Usuarios.findById(context.req.accessToken.userId, function(err, usuario){
+                
+                usuario.listaFamiliarId=listaCreada;
+                //Guardamos el usuario
+                usuario.save(function (err, usuario) {
+                    next();
+                  });
+            })
+
+            
         }else{
             console.log("El usuario no está logueado");
             next();
