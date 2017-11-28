cb328ff5f60ef2c016024818d555302e599f9ff4
Jose Manuel Zapata Martínez <joseicew@gmail.com>
2017-11-28 12:19:39 +0100

    Aceptar Solicitudes completado

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -28,4 +28,32 @@ module.exports = function(Listafamiliar) {
             next();
         }
     })
+    
+    /**
+    * "Solicitud a una lista"
+    * @param {object} contexto "context"
+    * @param {Function(Error)} callback
+    */
+   
+   Listafamiliar.prototype.solicitar = function(contexto, callback) {
+    var Solicitud;
+    var UsuId = contexto.req.accessToken.userId;
+    var listaID = this.id;
+    //tenemos que añadir este usuario a la lista que solicite, tenemos la lista y el usuario, añadamoslos
+    //this.id --> Recoge el id que ha introducido el usuario
+    this.solicita.add(UsuId,function(err) {
+        Solicitud = {
+            ListaFamiliarId: listaID,
+            UsuarioId: UsuId
+        }   
+        console.log(Solicitud);
+        callback(null,Solicitud);
+    });
+    
+     
+   };
+
+   
+
+
 };
