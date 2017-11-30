c46f139b0a4c04f2d253ec048610c9374bea4fc9
Jose Manuel Zapata Martínez <joseicew@gmail.com>
2017-11-30 12:10:40 +0100

    Terminado

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -2,33 +2,30 @@
 
 module.exports = function(Listafamiliar) {
     console.log();
+
+    Listafamiliar.beforeRemote('create', function(context, listaFamiliar, next) {
+        context.args.data.ownerId = context.req.accessToken.userId;
+        next();
+    })
+
+
     Listafamiliar.afterRemote('create', function(context, listaFamiliar, next) {
         //preparo el modelo usuarios
         var Usuarios = Listafamiliar.app.models.Usuario;
         var listaCreada = listaFamiliar.id;
         // Compruebo si está logueado
-        
-        if (context.req.accessToken && context.req.accessToken.userId) {
-            //Si está logueado le asocio a esa lista que acaba de crear su id como owner
-            
-            context.args.data.ownerId = context.req.accessToken.userId;
-            console.log(listaFamiliar.ownerId);
-            Usuarios.findById(context.req.accessToken.userId, function(err, usuario){
-                
-                usuario.listaFamiliarId=listaCreada;
-                //Guardamos el usuario
-                usuario.save(function (err, usuario) {
-                    next();
-                  });
-            })
 
+        
+        Usuarios.findById(context.req.accessToken.userId, function(err, usuario){
             
-        }else{
-            console.log("El usuario no está logueado");
+            usuario.listaFamiliarId=listaCreada;
+            //Guardamos el usuario
+            usuario.save();
             next();
-        }
+            
+        
+        })
     })
-    
     /**
     * "Solicitud a una lista"
     * @param {object} contexto "context"
@@ -54,6 +51,4 @@ module.exports = function(Listafamiliar) {
    };
 
    
-
-
 };
--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,57 @@
 'use strict';
 
 module.exports = function(Usuario) {
+    /**
+     * Aceptamos la solicitud de un usuario en nuestra lista
+     * @param {object} contexto Context
+     * @param {Function(Error, array)} callback
+     */
 
+    Usuario.prototype.aceptarSolicitud = function(contexto, callback) {
+        var ListaUsuarios; //Callback a rellenar
+        var idSolicitante = this.id; //Id entrante
+        var idMio =contexto.req.accessToken.userId; //Mi Id
+        var idMiLista ; //Tenemos que buscar que lista tengo asociada según mi ID
+        var Usuarios = this;
+        var miLista;
+        console.log("ID Solicitante: "+idSolicitante);
+        console.log("ID Mio: "+idMio);
+        
+
+        Usuario.findById(idMio, function(err, usuario){
+            if(err)callback(err);
+            idMiLista=usuario.listaFamiliarId;
+            console.log("Mi Id de lista: "+idMiLista);
+
+            Usuarios.solicita.findById(idMiLista, function(err, solicitud){
+                if(err)callback(err);
+                miLista=solicitud;
+                
+                console.log(solicitud);
+
+                Usuario.findById(idSolicitante, function(err,usuarioModificado){
+                    if(err)callback(err);
+                    usuarioModificado.listaFamiliarId = idMiLista;
+                    usuarioModificado.save();
+                    console.log(usuarioModificado);
+
+                    Usuario.find( {where:{"listaFamiliarId":idMiLista}} ,function(err, participantes) {
+                    if(err)callback(err);      
+                    console.log(participantes);
+                    ListaUsuarios = participantes;
+                    
+                    usuarioModificado.solicita.remove(miLista,function(err) {
+                      console.log("Borrando solicitud: "+idSolicitante);
+                      
+                    });
+                    
+                    callback(null, ListaUsuarios);
+                    });
+                })
+            })
+           
+        })
+
+    };
+  
 };
