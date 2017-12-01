c267677a797915970bf1c026dd8441da460d0cbd
Jose Manuel Zapata Martínez <joseicew@gmail.com>
2017-12-01 10:04:34 +0100

    Limpiar Solicitudes Terminado

--- common/models/producto.js
+++ common/models/producto.js
@@ -2,4 +2,45 @@
 
 module.exports = function(Producto) {
 
+    /**
+     * Ponemos a false todos los productos de la lista, indicando que están comprados
+     * @param {object} contexto Contexto - Context
+     * @param {Function(Error, object)} callback
+     */
+
+    Producto.limpiarLista = function(contexto, callback) {
+        var listaProductos; //callback
+        var Productos = this; //Instancia
+        var idMio =contexto.req.accessToken.userId; //Mi Id
+        var Usuarios = Producto.app.models.Usuario; //Modelo usuarios
+        var idLista; //Id de la lista que pertenece el usuario
+        var ListasFamiliares = Producto.app.models.ListaFamiliar;
+        var miLista; //Lista para modificar valores
+
+        //Teniendo el ID voy a buscar mi Id de lista
+        Usuarios.findById(idMio, function(err,usuario){
+            if (err) callback(err);
+            idLista = usuario.listaFamiliarId;
+            console.log(idLista);
+
+            //Ya sabiendo a que lsita pertenezco, voy a rescatarla
+            ListasFamiliares.findById(idLista, function(err,lista){
+                miLista = lista;
+                console.log(miLista);
+
+                Producto.find( {where:{"listaFamiliarId":miLista.id}},function(err,listaproductos){
+                    console.log(listaproductos);
+                    for(var i=0;i<listaproductos.length;i++){
+                        listaproductos[i].comprar=0;
+                    }
+                    console.log(listaproductos);
+                    listaProductos=listaproductos;
+                    callback(null, listaProductos);
+                })
+            })
+        })
+
+        
+    };
+    
 };
