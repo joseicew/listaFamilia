b888c35525a43a433bdc70788c842ee8db38c4fd
Jose Manuel Zapata Martínez <joseicew@gmail.com>
2017-12-03 11:20:00 +0100

    Fallo corregido, ahora si se modifica limpiarLista en la BD

--- common/models/producto.js
+++ common/models/producto.js
@@ -28,19 +28,74 @@ module.exports = function(Producto) {
                 miLista = lista;
                 console.log(miLista);
 
+                //obtenermos los productos de la lista
                 Producto.find( {where:{"listaFamiliarId":miLista.id}},function(err,listaproductos){
                     console.log(listaproductos);
                     for(var i=0;i<listaproductos.length;i++){
                         listaproductos[i].comprar=0;
+                        listaproductos[i].save();
                     }
+
                     console.log(listaproductos);
-                    listaProductos=listaproductos;
-                    callback(null, listaProductos);
+
+                    //listaproductos.save();
+                    
+                    callback(null, listaproductos);
                 })
             })
         })
 
         
     };
+
+    /**
+     * Cambiamos de estado el producto a true si es false o viceversa
+     * @param {object} contexto Contexto - Context
+     * @param {Function(Error, object)} callback
+     */
+
+    Producto.prototype.comprado = function(contexto, callback) {
+        var ListaProductosUsuario;
+        var Productos = this; //Instancia
+        var idMio =contexto.req.accessToken.userId; //Mi Id
+        var Usuarios = Producto.app.models.Usuario; //Modelo usuarios
+        var idLista; //Id de la lista que pertenece el usuario
+        var ListasFamiliares = Producto.app.models.ListaFamiliar;
+        var miLista; //Lista para modificar valores
+
+        //Teniendo el ID voy a buscar mi Id de lista
+        Usuarios.findById(idMio, function(err,usuario){
+            if (err) callback(err);
+            idLista = usuario.listaFamiliarId;
+            console.log(idLista);
+
+            //Ya sabiendo a que lsita pertenezco, voy a rescatarla
+            ListasFamiliares.findById(idLista, function(err,lista){
+                miLista = lista;
+                console.log(miLista);
+
+                //obtenermos los productos de la lista
+                Producto.find( {where:{"listaFamiliarId":miLista.id}},function(err,listaproductos){
+                    console.log(listaproductos);
+                    console.log(Productos.id);
+                    for(var i=0;i<listaproductos.length;i++){
+                        console.log(listaproductos[i].id+" "+Productos.id);
+                        if(listaproductos[i].id == Productos.id){
+                            console.log(listaproductos[i]);
+                            if(listaproductos[i].comprar==true){listaproductos[i].comprar=false;}
+                            else if(listaproductos[i].comprar==false){listaproductos[i].comprar=true;}                            
+                        }
+                        
+                    }
+                    
+                    console.log(listaproductos);
+                    ListaProductosUsuario=listaproductos;
+                    callback(null, ListaProductosUsuario);
+                })
+            })
+        })
+
+    };
+  
     
 };
